3345b4b5f38e0d1b69fbe576f037027454dd085c
System Administrator <root@yonemaru-no-MacBook-Air.local>
2016-06-25 04:26:21 +0900

    commit

--- web-push2/service-worker.js
+++ web-push2/service-worker.js
@@ -7,10 +7,10 @@ self.addEventListener('push', function(event) {
   // 動的にユーザーごとにメッセージを変えたい場合は、
   // ペイロードの暗号化を行うか、FetchAPIで動的に情報を取得する必要があります。
   var title = '新着記事のお知らせです';
-  var body = 'ServiceWorkerの記事を公開しました';
+  var body = '記事を公開しました';
   var icon = 'ics_logo_512x512.png';
   var tag = 'simple-push-demo-notification-tag';
-  var url = 'https://ics.media/entry/11763';
+  var url = 'http://www.yahoo.co.jp/';
 
   event.waitUntil(
     self.registration.showNotification(title, {
